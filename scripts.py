37582d2ea93f2fe15fc27dc9017db42e443a6135
Vitaliy Pavlenko <vpavlenko@iponweb.net>
2021-04-05 20:43:19 +0300

    add replace if conditions with custom exceptions and context decorators

--- scripts.py
+++ scripts.py
@@ -1,6 +1,8 @@
 import random
 
+from contextlib import contextmanager
 from datacenter.models import Chastisement, Commendation, Lesson, Mark, Schoolkid
+from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
 
 COMMENDATIONS = [
     "Молодец!",
@@ -36,47 +38,61 @@ COMMENDATIONS = [
 ]
 
 
+class IncorrectSchoolkidRequestError(Exception):
+    pass
+
+
+@contextmanager
+def print_friendly_errors():
+    try:
+        yield
+    except IncorrectSchoolkidRequestError as error:
+        print(error)
+
+
 def get_schoolkid(schoolkid_name):
-    schoolkid = Schoolkid.objects.filter(full_name__contains=schoolkid_name)
-    count = schoolkid.count()
-    if count == 1:
-        return schoolkid[0]
-    else:
-        print(f"Найдено {count} учеников с именем {schoolkid_name}\n"
+    try:
+        schoolkid = Schoolkid.objects.get(full_name__contains=schoolkid_name)
+    except ObjectDoesNotExist:
+        raise IncorrectSchoolkidRequestError(f"Не найдено ученика с именем {schoolkid_name}\n"
               f"Попробуйте другое имя")
+    except MultipleObjectsReturned:
+        raise IncorrectSchoolkidRequestError(f"Найдено больше одного ученика с именем {schoolkid_name}\n"
+              f"Попробуйте другое имя")
+    return schoolkid
 
 
+@print_friendly_errors()
 def fix_marks(schoolkid_name):
     schoolkid = get_schoolkid(schoolkid_name)
-    if schoolkid:
-        Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3]).update(points=5)
+    Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3]).update(points=5)
 
 
+@print_friendly_errors()
 def remove_chastisements(schoolkid_name):
     schoolkid = get_schoolkid(schoolkid_name)
-    if schoolkid:
-        Chastisement.objects.filter(schoolkid=schoolkid).delete()
+    Chastisement.objects.filter(schoolkid=schoolkid).delete()
 
 
+@print_friendly_errors()
 def create_commendation(schoolkid_name, subject_name):
     schoolkid = get_schoolkid(schoolkid_name)
-    if schoolkid:
-        last_lesson = Lesson.objects.filter(
-            year_of_study=schoolkid.year_of_study,
-            group_letter=schoolkid.group_letter,
-            subject__title__contains=subject_name,
-        ).order_by('-date').first()
+    last_lesson = Lesson.objects.filter(
+        year_of_study=schoolkid.year_of_study,
+        group_letter=schoolkid.group_letter,
+        subject__title__contains=subject_name,
+    ).order_by('-date').first()
 
-        if not last_lesson:
-            print(f"Уроков по предмету {subject_name} не найдено\n"
-                  f"Попробуйте другое название")
-            return
+    if not last_lesson:
+        print(f"Уроков по предмету {subject_name} не найдено\n"
+              f"Попробуйте другое название")
+        return
 
-        commendation_text = random.choice(COMMENDATIONS)
-        Commendation.objects.create(
-            text=commendation_text,
-            schoolkid=schoolkid,
-            subject=last_lesson.subject,
-            teacher=last_lesson.teacher,
-            created=last_lesson.date,
-        )
+    commendation_text = random.choice(COMMENDATIONS)
+    Commendation.objects.create(
+        text=commendation_text,
+        schoolkid=schoolkid,
+        subject=last_lesson.subject,
+        teacher=last_lesson.teacher,
+        created=last_lesson.date,
+    )
